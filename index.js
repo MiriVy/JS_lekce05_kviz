66f71ce698157c7e3d2c59723c4bf878a1bf732c
Miroslava Vybiralova <miroslava.vybiralova@gmail.com>
2024-02-26 16:36:07 +0100

    Moje reseni

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const questionElement = document.querySelector(".question")
+questionElement.textContent = "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon."
 
+let answer = prompt("Jaká je správná odpověď?")
+
+const answerTextElement = document.querySelector(".answer__text")
+answerTextElement.textContent = answer
+
+const answerElement = document.querySelector(".answer")
+
+if (answer === "Bell") {
+    answerElement.classList.add("answer--correct")
+}
+else {
+    answerElement.classList.add("answer--wrong")
+}
